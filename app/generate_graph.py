68e27cc2fee52d7df5142b954fd397528abac5e5
Jacobrakai <45674026+jacobyoby@users.noreply.github.com>
2023-05-03 14:48:50 -0400

    added tests (#6)

--- app/generate_graph.py
+++ app/generate_graph.py
@@ -1,27 +1,26 @@
-import pandas as pd
-import datetime
-import matplotlib.pyplot as plt
-from typing import List, Tuple
-import os
-
+import pandas as pd #Importing the pandas library for data manipulation
+import datetime #Importing the datetime module for date-time operations
+import matplotlib.pyplot as plt #Importing matplotlib to plot the data
+from typing import List, Tuple #Importing the list and tuple functions from the typing module
+import os #Importing the operating system module to handle file paths
 
 def read_stock_data_from_csv(file_path: str) -> Tuple[List[datetime.datetime], List[float], List[float]]:
     """
     This function reads stock data from a CSV file and returns a tuple containing the times, USD prices
     and CNY prices as three separate lists.
     """
-    try:
-        df = pd.read_csv(file_path)
-        if not {"Time", "Price in USD", "Price in CNY"}.issubset(set(df.columns)):
+    try: #Using a try-except statement to read the file and handle errors
+        df = pd.read_csv(file_path)  #Reading the file into a pandas DataFrame
+        if not {"Time", "Price in USD", "Price in CNY"}.issubset(set(df.columns)): #Checking for valid column names
             raise ValueError("Invalid input file format!")
-    except (FileNotFoundError, ValueError) as e:
+    except (FileNotFoundError, ValueError) as e: #Handling FileNotFoundError or ValueError exceptions
         print(f"{e}")
         return [], [], []
 
-    times = [datetime.datetime.strptime(t, "%Y-%m-%d %H:%M:%S.%f") for t in df["Time"]]
-    usd_prices = list(df["Price in USD"])
-    cny_prices = list(df["Price in CNY"])
-    return times, usd_prices, cny_prices
+    times = [datetime.datetime.strptime(t, "%Y-%m-%d %H:%M:%S.%f") for t in df["Time"]] #Converting strings to datetime objects
+    usd_prices = list(df["Price in USD"]) #Creating a list of USD prices
+    cny_prices = list(df["Price in CNY"]) #Creating a list of CNY prices
+    return times, usd_prices, cny_prices  #Returning the three lists
 
 
 def plot_stock_data(times: List[datetime.datetime], usd_prices: List[float], cny_prices: List[float], title: str,
@@ -29,14 +28,14 @@ def plot_stock_data(times: List[datetime.datetime], usd_prices: List[float], cny
     """
     This function generates a plot of the stock prices over time and saves it to a file.
     """
-    fig, ax = plt.subplots(figsize=(12, 6))
-    ax.plot(times, usd_prices, label="Price in USD")
-    ax.plot(times, cny_prices, label="Price in CNY")
-    ax.set(title=title, xlabel=xlabel, ylabel=ylabel)
-    ax.legend()
-    ax.grid()
-    fig.savefig(save_path)
-    plt.close()
+    fig, ax = plt.subplots(figsize=(12, 6)) #Creating a basic plot with labels
+    ax.plot(times, usd_prices, label="Price in USD") #Plotting the USD prices
+    ax.plot(times, cny_prices, label="Price in CNY") #Plotting the CNY prices
+    ax.set(title=title, xlabel=xlabel, ylabel=ylabel) #Setting the plot labels
+    ax.legend() #Adding the legend
+    ax.grid() #Creating a grid
+    fig.savefig(save_path) #Saving the plot to a file
+    plt.close(fig) #Closing the figure
 
 
 def main(input_file: str, output_file: str, title: str = "Stock Price Chart", xlabel: str = "Time",
@@ -44,13 +43,13 @@ def main(input_file: str, output_file: str, title: str = "Stock Price Chart", xl
     """
     This is the main function that reads the stock data and plots a graph using the data.
     """
-    times, usd_prices, cny_prices = read_stock_data_from_csv(input_file)
-    if not all([times, usd_prices, cny_prices]):
+    times, usd_prices, cny_prices = read_stock_data_from_csv(input_file) #Retrieving the stock data
+    if not all([times, usd_prices, cny_prices]): #Checking that all data has been successfully loaded
         return
-    plot_stock_data(times, usd_prices, cny_prices, title, xlabel, ylabel, output_file)
+    plot_stock_data(times, usd_prices, cny_prices, title, xlabel, ylabel, output_file) #Generating the plot
 
 
 if __name__ == "__main__":
-    input_file = "app/data/stock_data.csv"
-    output_file = "app/data/stock_price_chart.png"
-    main(input_file, output_file)
+    input_file = os.path.join("app", "data", "stock_data.csv")  #Setting the path of the input file
+    output_file = os.path.join("app", "data", "stock_price_chart.png") #Setting the path of the output file
+    main(input_file, output_file) #Running the main function
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import csv
-import os
 import datetime
+import os
 import urllib.parse
 from typing import Union
 
@@ -9,55 +9,50 @@ from dotenv import load_dotenv
 from forex_python.converter import CurrencyRates
 from generate_graph import main as generate_graph_main
 
-
 # Load the .env file
 load_dotenv()
 
+# Set constant values
 ALPHAVANTAGE_API_URL = "https://www.alphavantage.co/query"
 STOCK_SYMBOL: str = os.getenv("STOCK_SYMBOL", None)
 API_KEY: str = os.getenv("API_KEY", None)
-
 file_name: str = "stock_data.csv"
 
-
-def fetch_stock_price() -> Union[None, float]:
-    """
-    This function fetches the current stock price for a given symbol and returns it as a float.
-    """
+def fetch_stock_price(stock_symbol: str, api_key: str) -> Union[None, float]:
     params = {
         "function": "GLOBAL_QUOTE",
-        "symbol": STOCK_SYMBOL,
-        "apikey": API_KEY,
+        "symbol": stock_symbol,
+        "apikey": api_key,
     }
-    
-    print(f"API_KEY: {API_KEY}")
-    print(f"STOCK_SYMBOL: {STOCK_SYMBOL}")
     url: str = f"{ALPHAVANTAGE_API_URL}?{urllib.parse.urlencode(params)}"
+
     response = requests.get(url)
     json_data = response.json()
+
     current_price = json_data.get("Global Quote", {}).get("05. price")
+
     if current_price is not None:
         return float(current_price)
     print(f"Unable to fetch the stock price. JSON data: {json_data}")
     return None
 
-
-def convert_usd_to_cny(usd_amount: float) -> float:
-    """
-    This function converts an amount in USD to CNY and returns it as a float.
-    """
+def convert_usd_to_cny(price: float) -> float:
     currency_rates = CurrencyRates()
-    return currency_rates.convert("USD", "CNY", usd_amount)
+    return currency_rates.convert("USD", "CNY", price)
 
-
-def save_stock_data_to_csv(time: datetime.datetime, usd_price: float, cny_price: float):
+def save_stock_data_to_csv(file_path: str, time: datetime.datetime, usd_price: float, cny_price: float):
     """
     This function saves the time, USD price and CNY price to a CSV file.
     """
+    # Define CSV headers
     headers = ["Time", "Price in USD", "Price in CNY"]
-    file_path: str = f"app/data/{file_name}"
-    if not os.path.exists("app/data"):
-        os.makedirs("app/data")
+
+    # Create directory if it doesn't exist
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Check if file exists and write data to the file
     file_exists = os.path.isfile(file_path)
     with open(file_path, "a", newline="") as file:
         writer = csv.writer(file)
@@ -67,20 +62,17 @@ def save_stock_data_to_csv(time: datetime.datetime, usd_price: float, cny_price:
 
 
 def main():
-    """
-    This is the main function that fetches the stock price, converts it to CNY, saves it to a CSV file
-    and generates a graph using the saved data.
-    """
-    stock_price_usd: Union[None, float] = fetch_stock_price()
+    stock_price_usd: Union[None, float] = fetch_stock_price(stock_symbol=STOCK_SYMBOL, api_key=API_KEY)
+
     if stock_price_usd is not None:
-        stock_price_cny: float = convert_usd_to_cny(stock_price_usd)
+        stock_price_cny: float = convert_usd_to_cny(price=stock_price_usd)
         print(f"Current price of TSE stock in USD: {stock_price_usd}")
         print(f"Current price of TSE stock in CNY: {stock_price_cny}")
         current_time = datetime.datetime.now()
-        save_stock_data_to_csv(current_time, stock_price_usd, stock_price_cny)
-        generate_graph_main(input_file='app/data/stock_data.csv', output_file='app/data/stock_price_chart.png')
-
-
+        save_stock_data_to_csv(file_path=f"app/data/{file_name}", time=current_time, usd_price=stock_price_usd, cny_price=stock_price_cny)
+        
+        generate_graph_main(input_file='app/data/stock_data.csv',
+                            output_file='app/data/stock_price_chart.png')
 
 if __name__ == "__main__":
     main()
